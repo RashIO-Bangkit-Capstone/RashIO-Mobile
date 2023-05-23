eb89f7c469095198a52a9d8ed1785195ed390240
Boreneoux <ichlasul.ap@gmail.com>
2023-05-23 17:52:28 +0700

    project initialization

---
+++ app/src/main/java/id/rashio/android/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.rashio.android.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.rashio.android.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
