bff790fce0f7ab41c72a8f3226c648d6bee5c4ac
Ichlasul Fikri <ichlasul.ap@gmail.com>
2023-06-13 00:24:26 +0700

    User data on profile screen

--- app/src/main/java/id/rashio/android/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/id/rashio/android/ui/main/profile/ProfileFragment.kt
@@ -1,12 +1,14 @@
 package id.rashio.android.ui.main.profile
 
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.addCallback
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -32,6 +34,7 @@ class ProfileFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.navView.setupWithNavController(findNavController())
@@ -45,6 +48,10 @@ class ProfileFragment : Fragment() {
             Toast.makeText(activity, "Berhasil Logout", Toast.LENGTH_SHORT).show()
             findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentToLoginFragment())
         }
+
+        binding.tvUsername.text = viewModel.getUserName()
+        binding.tvEmailUser.text = viewModel.getEmail()
+        binding.tvPhoneNumber.text = viewModel.getPhoneNumber()
     }
 
     override fun onStart() {
--- app/src/main/java/id/rashio/android/ui/main/profile/ProfileViewModel.kt
+++ app/src/main/java/id/rashio/android/ui/main/profile/ProfileViewModel.kt
@@ -1,6 +1,8 @@
 package id.rashio.android.ui.main.profile
 
 import android.content.SharedPreferences
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import id.rashio.android.api.ApiService
@@ -13,10 +15,28 @@ class ProfileViewModel @Inject constructor(
     private val sharedPreferences: SharedPreferences
 ) : ViewModel() {
 
+    private val tokenManager = TokenManager(sharedPreferences)
+
+
     fun logout(){
         val editor = sharedPreferences.edit()
         editor.remove("ACCESS_TOKEN")
         editor.remove("REFRESH_TOKEN")
         editor.apply()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getUserName(): String? {
+        return tokenManager.getName()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getEmail(): String? {
+        return tokenManager.getEmail()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getPhoneNumber(): String? {
+        return tokenManager.getPhoneNumber()
     }
 }
